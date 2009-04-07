3e32b1c14788c0fdf20c7dc422ff080cbc2784e8
Lars Christensen <lac@gatehouse.dk>
2009-04-07 16:59:42 +0200

    Refactored to be non-polluting and self-hostable

--- Rakefile
+++ Rakefile
@@ -16,4 +16,31 @@ end
 
 task :test => :stub
 
+task :standalone => [ 'bin/ocrasa.rb', 'bin/ocrasa.exe' ]
+
+
+file 'bin/ocrasa.rb' => [ 'bin/ocra.rb', 'share/ocra/stub.exe', 'share/ocra/lzma.exe' ] do
+  cp 'bin/ocra.rb', 'bin/ocrasa.rb'
+  File.open("bin/ocrasa.rb", "a") do |f|
+    f.puts "__END__"
+    stub = File.open("share/ocra/stub.exe", "rb") {|g| g.read}
+    stub64 = [stub].pack("m")
+    f.puts stub64.size
+    f.puts stub64
+    lzma = File.open("share/ocra/lzma.exe", "rb") {|g| g.read}
+    lzma64 = [lzma].pack("m")
+    f.puts lzma64.size
+    f.puts lzma64
+  end
+end
+
+file 'bin/ocrasa.exe' => [ 'bin/ocra.rb', 'bin/ocrasa.rb' ] do
+  sh "ruby bin/ocra.rb bin/ocrasa.rb"
+end
+
+task :clean do
+  rm_rf Dir.glob("bin/*.exe")
+  sh "mingw32-make -C src clean"
+end
+
 # vim: syntax=Ruby
--- bin/ocra.rb
+++ bin/ocra.rb
@@ -9,26 +9,82 @@ module Ocra
   OP_CREATE_PROCESS = 3
   OP_DECOMPRESS_LZMA = 4
   OP_SETENV = 5
-  
-  def Ocra.find_images
+
+  class << self
+    attr_accessor :lzma_mode
+    attr_accessor :extra_dlls
+    attr_accessor :files
+    attr_accessor :load_autoload
+    attr_accessor :force_windows
+    attr_accessor :force_console
+    attr_accessor :quiet
+    attr_reader :lzmapath
+    attr_reader :sebimage
+  end
+
+  def Ocra.initialize_ocra
+    @lzma_mode = true
+    @extra_dlls = []
+    @files = []
+    @load_autoload = true
+    
     if defined?(DATA)
-      $sebimage = DATA.read(DATA.readline.to_i).unpack("m")[0]
-      $lzmaimage = DATA.read(DATA.readline.to_i).unpack("m")[0]
-      $lzmapath = File.join(ENV['TEMP'], 'lzma.exe').tr('/','\\')
-      puts $lzmapath
-      puts $lzmaimage.size
-      File.open($lzmapath, "wb") { |f| f << $lzmaimage }
+      @sebimage = DATA.read(DATA.readline.to_i).unpack("m")[0]
+      lzmaimage = DATA.read(DATA.readline.to_i).unpack("m")[0]
+      @lzmapath = File.join(ENV['TEMP'], 'lzma.exe').tr('/','\\')
+      File.open(@lzmapath, "wb") { |f| f << lzmaimage }
     else
-      $sebimage = File.open(File.join(File.dirname(__FILE__), '../share/ocra/stub.exe'), "rb") { |f| f.read }
-      $lzmapath = File.join(File.dirname(__FILE__), '../share/ocra/lzma.exe')
-      raise "lzma.exe not found" unless File.exist?($lzmapath)
+      @sebimage = File.open(File.join(File.dirname(__FILE__), '../share/ocra/stub.exe'), "rb") { |f| f.read }
+      @lzmapath = File.expand_path('../share/ocra/lzma.exe', File.dirname(__FILE__)).tr('/','\\')
+      raise "lzma.exe not found" unless File.exist?(@lzmapath)
+    end
+  end
+
+  def Ocra.parseargs(argv)
+    usage = <<EOF
+ocra [--dll dllname] [--no-lzma] script.rb
+
+--dll dllname    Include additional DLLs from the Ruby bindir.
+--no-lzma        Disable LZMA compression of the executable.
+--quiet          Suppress output.
+--help           Display this information.
+--windows        Force Windows application (rubyw.exe)
+--console        Force console application (ruby.exe)
+--no-autoload    Don't load/include script.rb's autoloads
+EOF
+
+    while arg = argv.shift
+      case arg
+      when /\A--(no-)?lzma\z/
+        Ocra.lzma_mode = !$1
+      when /\A--dll\z/
+        Ocra.extra_dlls << argv.shift
+      when /\A--quiet\z/
+        Ocra.quiet = true
+      when /\A--windows\z/
+        Ocra.force_windows = true
+      when /\A--console\z/
+        Ocra.force_console = true
+      when /\A--no-autoload\z/
+        Ocra.load_autoload = false
+      when /\A--help\z/, /\A--/
+        puts usage
+        exit
+      else
+        @files << arg
+      end
+    end
+
+    if Ocra.files.empty?
+      puts usage
+      exit
     end
   end
   
   def Ocra.build_exe
     libs = []
 
-    if $autoload
+    if Ocra.load_autoload
       # Force loading autoloaded
       modules_checked = []
       loop do
@@ -56,7 +112,7 @@ module Ocra
 
     require 'rbconfig'
     exec_prefix = RbConfig::CONFIG['exec_prefix']
-    src_prefix = File.expand_path(File.dirname($files[0]))
+    src_prefix = File.expand_path(File.dirname(Ocra.files[0]))
     sitelibdir = RbConfig::CONFIG['sitelibdir']
     instsitelibdir = sitelibdir[exec_prefix.size+1..-1]
 
@@ -84,24 +140,23 @@ module Ocra
       gemspecs = []
     end
 
-    find_images
     require 'rbconfig'
     bindir = RbConfig::CONFIG['bindir']
     libruby_so = RbConfig::CONFIG['LIBRUBY_SO']
 
-    executable = $files[0].sub(/(\.rbw?)?$/, '.exe')
+    executable = Ocra.files[0].sub(/(\.rbw?)?$/, '.exe')
 
-    puts "=== Building #{executable}" unless $quiet
+    puts "=== Building #{executable}" unless Ocra.quiet
     SebBuilder.new(executable) do |sb|
       sb.mkdir('src')
 
-      $files.each do |file|
+      Ocra.files.each do |file|
         path = File.join('src', file).tr('/','\\')
         sb.createfile(file, path)
       end
       sb.mkdir('bin')
       
-      if ($files[0] =~ /\.rbw$/ && !$force_windows) || $force_console
+      if (Ocra.files[0] =~ /\.rbw$/ && !Ocra.force_windows) || Ocra.force_console
         rubyexe = "ruby.exe"
       else
         rubyexe = "ruby.exe"
@@ -110,7 +165,7 @@ module Ocra
       sb.createfile(File.join(bindir, rubyexe), "bin\\" + rubyexe)
       
       sb.createfile(File.join(bindir, libruby_so), "bin\\#{libruby_so}")
-      $extra_dlls.each { |dll|
+      Ocra.extra_dlls.each { |dll|
         sb.createfile(File.join(bindir, dll), File.join("bin", dll).tr('/','\\'))
       }
       #sb.createfile('c:\lang\Ruby-186-27\lib\ruby\gems\1.8\specifications\wxruby-2.0.0-x86-mswin32-60.gemspec',
@@ -134,42 +189,47 @@ module Ocra
 
       sb.setenv('RUBYOPT', '')
       sb.setenv('RUBYLIB', '')
-      sb.createprocess("bin\\" + rubyexe, "#{rubyexe} \xff\\src\\" + $files[0])
-      puts "=== Compressing" unless $quiet or not $lzma_mode
+      sb.createprocess("bin\\" + rubyexe, "#{rubyexe} \xff\\src\\" + Ocra.files[0])
+      puts "=== Compressing" unless Ocra.quiet or not Ocra.lzma_mode
     end
-    puts "=== Finished (Final size was #{File.size(executable)})" unless $quiet
+    puts "=== Finished (Final size was #{File.size(executable)})" unless Ocra.quiet
   end
   
   class SebBuilder
     def initialize(path)
       @paths = {}
       File.open(path, "wb") do |f|
-        f.write($sebimage)
-        if $lzma_mode
+        f.write(Ocra.sebimage)
+        if Ocra.lzma_mode
           @of = ""
         else
           @of = f
         end
         yield(self)
 
-        if $lzma_mode
-          File.open("tmpin", "wb") { |tmp| tmp.write(@of) }
-          system("#{$lzmapath} e tmpin tmpout 2>NUL") or fail
-          @c = File.open("tmpout", "rb") { |tmp| tmp.read }
-          f.write([OP_DECOMPRESS_LZMA, @c.size, @c].pack("VVA*"))
-          f.write([OP_END].pack("V"))
+        if Ocra.lzma_mode
+          begin
+            File.open("tmpin", "wb") { |tmp| tmp.write(@of) }
+            system("#{Ocra.lzmapath} e tmpin tmpout 2>NUL") or fail
+            @c = File.open("tmpout", "rb") { |tmp| tmp.read }
+            f.write([OP_DECOMPRESS_LZMA, @c.size, @c].pack("VVA*"))
+            f.write([OP_END].pack("V"))
+          ensure
+            File.unlink("tmpin") if File.exist?("tmpin")
+            File.unlink("tmpout") if File.exist?("tmpout")
+          end
         else
-          f.write(@of) if $lzma_mode
+          f.write(@of) if Ocra.lzma_mode
         end
 
         f.write([OP_END].pack("V"))
-        f.write([$sebimage.size].pack("V"))
+        f.write([Ocra.sebimage.size].pack("V"))
         f.write(Signature.pack("C*"))
       end
     end
     def mkdir(path)
       @paths[path] = true
-      puts "m #{path}" unless $quiet
+      puts "m #{path}" unless Ocra.quiet
       @of << [OP_CREATE_DIRECTORY, path].pack("VZ*")
     end
     def ensuremkdir(tgt)
@@ -182,15 +242,15 @@ module Ocra
     def createfile(src, tgt)
       ensuremkdir(File.dirname(tgt))
       str = File.open(src, "rb") { |s| s.read }
-      puts "a #{tgt}" unless $quiet
+      puts "a #{tgt}" unless Ocra.quiet
       @of << [OP_CREATE_FILE, tgt, str.size, str].pack("VZ*VA*")
     end
     def createprocess(image, cmdline)
-      puts "l #{image} #{cmdline}" unless $quiet
+      puts "l #{image} #{cmdline}" unless Ocra.quiet
       @of << [OP_CREATE_PROCESS, image, cmdline].pack("VZ*Z*")
     end
     def setenv(name, value)
-      puts "e #{name} #{value}" unless $quiet
+      puts "e #{name} #{value}" unless Ocra.quiet
       @of << [OP_SETENV, name, value].pack("VZ*Z*")
     end
     def close
@@ -200,60 +260,20 @@ module Ocra
   
 end # module Ocra
 
-$lzma_mode = true
-$extra_dlls = []
-$files = []
-$autoload = true
-
-usage = <<EOF
-ocra [--dll dllname] [--no-lzma] script.rb
-
---dll dllname    Include additional DLLs from the Ruby bindir.
---no-lzma        Disable LZMA compression of the executable.
---quiet          Suppress output.
---help           Display this information.
---windows        Force Windows application (rubyw.exe)
---console        Force console application (ruby.exe)
---no-autoload    Don't load/include script.rb's autoloads
-EOF
-
-while arg = ARGV.shift
-  case arg
-  when /\A--(no-)?lzma\z/
-    $lzma_mode = !$1
-  when /\A--dll\z/
-    $extra_dlls << ARGV.shift
-  when /\A--quiet\z/
-    $quiet = true
-  when /\A--windows\z/
-    $force_windows = true
-  when /\A--console\z/
-    $force_console = true
-  when /\A--no-autoload\z/
-    $autoload = false
-  when /\A--help\z/, /\A--/
-    puts usage
-    exit
-  else
-    $files << arg
-  end
-end
-
-if $files.empty?
-  puts usage
-  exit
-end
-
 if defined? Gem
   puts "=== Warning: Rubygems is loaded. Rubygems will be included the archive."
   puts "        RUBYOPT=#{ENV['RUBYOPT']}"
 end
 
-at_exit do
-  Ocra.build_exe
-  exit
+if File.basename(__FILE__) == File.basename($0)
+  Ocra.initialize_ocra
+  Ocra.parseargs(ARGV)
+  puts "=== Loading script to check dependencies" unless Ocra.quiet
+  $0 = "<ocra>"
+  ARGV.clear
+  at_exit do
+    Ocra.build_exe
+    exit
+  end
+  load Ocra.files[0]
 end
-
-puts "=== Loading script to check dependencies" unless $quiet
-load $files[0]
-
