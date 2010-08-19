0cb315310a93551fb513d0fb6ec48606ec6778af
Lars Christensen <larsch@belunktum.dk>
2010-08-19 19:14:35 +0200

    The right OCRA way to do this on any ruby version

--- test/fixtures/relativerequire/relativerequire.rb
+++ test/fixtures/relativerequire/relativerequire.rb
@@ -1,4 +1,4 @@
-$: << "." if RUBY_VERSION >= "1.9.2"
-require 'somedir/somefile.rb'
-require 'SomeDir/otherfile.rb'
+$:.unshift File.dirname(__FILE__)
+require 'somedir/somefile'
+require 'SomeDir/otherfile'
 exit 160 if __FILE__ == $0 and defined?(SomeConst) and defined?(OtherConst)
