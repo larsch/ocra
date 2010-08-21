26b819094a87a3f701dbe96917220462970bd359
Lars Christensen <larsch@belunktum.dk>
2010-08-21 12:37:50 +0200

    Workarounds for Ruby 1.8.6

--- test/fixtures/arguments/arguments.rb
+++ test/fixtures/arguments/arguments.rb
@@ -1,5 +1,11 @@
 exit if defined?(Ocra)
 exit 1 if ARGV.size != 2
 exit 2 if ARGV[0] != "foo"
-exit 3 if ARGV[1] != "bar baz \"quote\""
+if RUBY_VERSION == "1.8.6"
+  # Ruby 1.8.6 has a command line quote-parsing bug that leaves extra
+  # chars after the argument
+  exit 3 if ARGV[1].index("bar baz \"quote\"") != 0
+else
+  exit 3 if ARGV[1] != "bar baz \"quote\""
+end
 exit 5
