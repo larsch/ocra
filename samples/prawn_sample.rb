5ca9a7d141a14c2c25fb665c83ce185ee3ff7ce2
Lars Christensen <larsch@belunktum.dk>
2010-08-29 14:00:29 +0200

    Updated prawn sample

--- samples/prawn_sample.rb
+++ samples/prawn_sample.rb
@@ -1,2 +1,7 @@
 require 'prawn'
 exit if defined?(Ocra)
+Prawn::Document.generate('prawn_sample.pdf') do
+  text "Hello, World!"
+  font.instance_eval { find_font("Helvetica.afm") } or fail
+end
+File.unlink("prawn_sample.pdf")
