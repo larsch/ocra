6fcf63a21f426f4fae76f74a9e2ec70a522b4664
John Mair <jrmair@gmail.com>
2017-12-12 18:16:02 +0100

    RVA means relative virtual address not relative virtual offset

--- src/stub.c
+++ src/stub.c
@@ -381,7 +381,7 @@ int CALLBACK _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCm
 static PIMAGE_NT_HEADERS retrieveNTHeader(LPVOID ptr) {
   PIMAGE_DOS_HEADER dosHeader = (PIMAGE_DOS_HEADER)ptr;
 
-  /* e_lfanew is an RVA (relative virtual offset) to the NTHeader (aka PE Header)
+  /* e_lfanew is an RVA (relative virtual address, i.e offset) to the NTHeader
    to get a usable pointer we add the RVA to the base address */
   return (PIMAGE_NT_HEADERS)((DWORD)dosHeader + (DWORD)dosHeader->e_lfanew);
 }
